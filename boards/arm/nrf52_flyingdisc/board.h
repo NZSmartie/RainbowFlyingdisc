e9f6f5dd598891d86597e52b3894a16d52476a57
NZSmartie <nzsmartie@gmail.com>
2018-11-24 21:44:22 +1300

    Remap SW0..SW3 and LED0..LED3 to leave lower 24 pin mappings for PWM LEDs

--- boards/arm/nrf52_flyingdisc/board.h
+++ boards/arm/nrf52_flyingdisc/board.h
@@ -5,39 +5,39 @@
 #include <soc.h>
 
 /* Push button switch 0 */
-#define SW0_GPIO_PIN     13
+#define SW0_GPIO_PIN     24
 #define SW0_GPIO_NAME    CONFIG_GPIO_P0_DEV_NAME
 #define SW0_GPIO_PIN_PUD GPIO_PUD_PULL_UP
 
 /* Push button switch 1 */
-#define SW1_GPIO_PIN     14
+#define SW1_GPIO_PIN     25
 #define SW1_GPIO_NAME    CONFIG_GPIO_P0_DEV_NAME
 #define SW1_GPIO_PIN_PUD GPIO_PUD_PULL_UP
 
 /* Push button switch 2 */
-#define SW2_GPIO_PIN     15
+#define SW2_GPIO_PIN     26
 #define SW2_GPIO_NAME    CONFIG_GPIO_P0_DEV_NAME
 #define SW2_GPIO_PIN_PUD GPIO_PUD_PULL_UP
 
 /* Push button switch 3 */
-#define SW3_GPIO_PIN     16
+#define SW3_GPIO_PIN     27
 #define SW3_GPIO_NAME    CONFIG_GPIO_P0_DEV_NAME
 #define SW3_GPIO_PIN_PUD GPIO_PUD_PULL_UP
 
 /* Onboard GREEN LED 0 */
-#define LED0_GPIO_PIN  17
+#define LED0_GPIO_PIN  28
 #define LED0_GPIO_PORT CONFIG_GPIO_P0_DEV_NAME
 
 /* Onboard GREEN LED 1 */
-#define LED1_GPIO_PIN  18
+#define LED1_GPIO_PIN  29
 #define LED1_GPIO_PORT CONFIG_GPIO_P0_DEV_NAME
 
 /* Onboard GREEN LED 2 */
-#define LED2_GPIO_PIN  19
+#define LED2_GPIO_PIN  30
 #define LED2_GPIO_PORT CONFIG_GPIO_P0_DEV_NAME
 
 /* Onboard GREEN LED 3 */
-#define LED3_GPIO_PIN  20
+#define LED3_GPIO_PIN  31
 #define LED3_GPIO_PORT CONFIG_GPIO_P0_DEV_NAME
 
 #endif /* __INC_BOARD_H */
